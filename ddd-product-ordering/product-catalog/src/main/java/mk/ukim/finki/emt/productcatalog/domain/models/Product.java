81d35785d902ff3c2ec4b1c735b44d6eddb73bc1
AnaGeorgieska <68482597+AnaGeorgieska@users.noreply.github.com>
2022-09-05 23:48:05 +0200

    First commit

---
+++ ddd-product-ordering/product-catalog/src/main/java/mk/ukim/finki/emt/productcatalog/domain/models/Product.java
@@ -0,0 +1,42 @@
+package mk.ukim.finki.emt.productcatalog.domain.models;
+
+import lombok.Getter;
+import mk.ukim.finki.emt.sharedkernel.domain.base.AbstractEntity;
+import mk.ukim.finki.emt.sharedkernel.domain.financial.Money;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+@Getter
+public class Product extends AbstractEntity<ProductId> {
+
+    private String productName;
+    private int sales=0;
+    @AttributeOverrides({
+            @AttributeOverride(name="amount", column = @Column(name = "price_amount")),
+            @AttributeOverride(name="currency", column = @Column(name = "price_currency")),
+    })
+    private Money price;
+
+    private Product(){
+        super(ProductId.randomId(ProductId.class));
+    }
+
+    public static Product build(String productName, Money price, int sales)
+    {
+        Product product=new Product();
+        product.price=price;
+        product.productName=productName;
+        product.sales=sales;
+        return product;
+    }
+    public void addSales(int qty) {
+        this.sales = this.sales - qty;
+    }
+
+    public void removeSales(int qty) {
+        this.sales -= qty;
+    }
+
+}
